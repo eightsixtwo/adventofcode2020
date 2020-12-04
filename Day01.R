dbfc8aa0325e7bc1fa32ee6bb467649ceda5af05
Anthony Foster <anthfoster862@gmail.com>
2020-12-04 13:44:17 +0000

    Day 01 complete

---
+++ Day01.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+# read in data
+data1 <- read_lines(file = "D01_input.txt")
+# create identical copies
+dataA <- data1
+dataB <- data1
+# create df of all possible combinations
+df1 <- crossing(dataA, dataB)
+# change data type
+df1$dataA <- as.numeric(df1$dataA)
+df1$dataB <- as.numeric(df1$dataB)
+# perform sum and multiply, then filter for 2020
+df2 <- df1 %>%
+  mutate(tot = dataA + dataB,
+         mul = dataA * dataB) %>%
+  filter(tot == 2020)
+# view result
+df2
